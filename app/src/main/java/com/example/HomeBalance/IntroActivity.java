22ca37cc1c5b7a15fa5b9390f32caad824199e96
Anthony1198 <anthony.schaeufele@yahoo.de>
2021-06-08 16:52:24 +0200

    DatenEingabe begonnen

--- app/src/main/java/com/example/HomeBalance/IntroActivity.java
+++ app/src/main/java/com/example/HomeBalance/IntroActivity.java
@@ -16,18 +16,15 @@ public class IntroActivity extends AppIntro2 {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-
         addSlide(AppIntroFragment.newInstance(getString(R.string.introTitle1), getString(R.string.introDescr1), R.drawable.intro1, Color.BLACK));
-
         addSlide(AppIntroFragment.newInstance(getString(R.string.introTitle2), getString(R.string.introDescr2), R.drawable.intro2, Color.BLACK));
-
-        setImmersiveMode();
+        addSlide(new DatenEingabeFragment());
     }
 
     @Override
     protected void onSkipPressed(Fragment currentFragment) {
         super.onSkipPressed(currentFragment);
-        Intent intent = new Intent(getApplicationContext(),DatenEingabe.class);
+        Intent intent = new Intent(getApplicationContext(),MainActivity.class);
         startActivity(intent);
     }
 
