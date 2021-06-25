cfbf2ca91c55982ede0e48c5831305dd6135e537
Jonah Schierding <jonah.schierding@live.de>
2021-06-25 15:43:05 +0200

    Nicht genutzte Imports entfernt

--- app/src/main/java/com/example/HomeBalance/MainActivity.java
+++ app/src/main/java/com/example/HomeBalance/MainActivity.java
@@ -3,16 +3,13 @@ package com.example.HomeBalance;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+
 
 
 public class MainActivity extends AppCompatActivity {
